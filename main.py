66ea988105656f69d8420ad687a3453424cff3da
Allison Valdez <allison.snipe@gmail.com>
2026-05-23 02:42:29 -0700

    tests

--- main.py
+++ main.py
@@ -53,10 +53,10 @@ print("\n\n--- Order Details Data ---")
 print(order_details)
 
 # 9. Find the total amount for all orders, calculated as the sum of rounded total prices. The total price for each order is the priceEach multiplied by the quantityOrdered.
-sum_total_price = conn.execute("""
-    SELECT SUM(ROUND(priceEach) * quantityOrdered)
+sum_total_price = pd.read_sql("""
+    SELECT ROUND(priceEach * quantityOrdered) AS total_price
     FROM orderDetails
-""").fetchone()
+""", conn).sum()
 print("\n\nResults of sum_total_price:")
 print(sum_total_price)
 
